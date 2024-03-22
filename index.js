aa8dfd42262588f41d19d8a59bddac754b503db5
Stacy <stacykimilu@gmail.com>
2024-03-22 12:53:10 +0300

    scope lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName ='Bob';
+var customerName = 'bob';
+
+function returnCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer() {
+    throw new Error('Assignment to constant variable.');
+}
+
+console.log({
+  returnCustomerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+});
